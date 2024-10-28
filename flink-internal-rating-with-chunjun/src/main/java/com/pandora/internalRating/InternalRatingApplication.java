d0eb7e3243d7275f0a3a2e3358e9f98efeb4a23e
CPZ <cpz77585211@163.com>
2024-10-28 17:05:41 +0800

    add code.

--- flink-internal-rating-with-chunjun/src/main/java/com/pandora/internalRating/InternalRatingApplication.java
+++ flink-internal-rating-with-chunjun/src/main/java/com/pandora/internalRating/InternalRatingApplication.java
@@ -1,4 +1,59 @@
 package com.pandora.internalRating;
 
+import com.pandora.internalRating.conf.Config;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+import java.time.LocalDate;
+
+/**
+ * 上一个版本中，改成了状态中不能只保存一个最终结果，还是要保存value的每一条，但是也不是全保留在状态中
+ *
+ * if state 中的min_submittime < currentdate - 30
+ * then 新来的数据的submintime < min_submittine则什么都不做
+ *      新来的数据的submittime > min_submittime扔到状态中，找到currentdate - 30最近的一个min_submittime，小于这个min_subittime的数据扔掉
+ */
 public class InternalRatingApplication {
+    private static String SUBMITTIME_LOCALDATE_PATTERN = "yyyyMMdd";
+
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+        /**
+         * 首先利用chujun的oracle-x增量读取数据
+         */
+
+        tableEnv.executeSql("create table bond_company_history\n" +
+                "code string,\n" +
+                "name string,\n" +
+                "type string, " +
+                "rate string, " +
+                "ratetype decimal(10, 0), " +
+                "submittime string, " +
+                "rankupdatedate string, " +
+                "inputid string, " +
+                "mktcode string, " +
+                "mktcode01 decimal(10, 0), " +
+                "watchrate string, " +
+                "expectrate string, " +
+                "isindraftdown decimal(10, 0), " +
+                "afterdownrating string, " +
+                "updatetimestamp timestamp(3), " +
+                "tim decimal(10, 0) " +
+                ") with (" +
+                " 'connector' = 'oracle-x', " +
+                " 'url' = '" + Config.sourceUrl + "', " +
+                " 'table-name' = '" + Config.sourceTableName + "', " +
+                " 'username' = '" + Config.sourceUserName + "', " +
+                " 'password' = '" + Config.sourcePassword + "', " +
+                " 'scan.polling-interval' = '30000', " +
+                " 'scan.increment.column' = 'updatetimestamp', " +
+                " 'scan.increment.column-type' = 'timestamp(3)' " +
+                ")");
+
+
+    }
 }
---
+++ flink-internal-rating-with-chunjun/src/main/java/com/pandora/internalRating/bean/BondInfoBean.java
@@ -0,0 +1,140 @@
+package com.pandora.internalRating.bean;
+
+public class BondInfoBean {
+    String code;
+    String name;
+    String type;
+    String rate;
+    String ratetype;
+    String submittime;
+    String rankupdatedate;
+    String inputid;
+    String mktcode01;
+    String watchrate;
+    String expectrate;
+    String isindraftdown;
+    String afterdownrating;
+    String updatetimestamp;
+    String tim;
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+
+    public String getRatetype() {
+        return ratetype;
+    }
+
+    public void setRatetype(String ratetype) {
+        this.ratetype = ratetype;
+    }
+
+    public String getSubmittime() {
+        return submittime;
+    }
+
+    public void setSubmittime(String submittime) {
+        this.submittime = submittime;
+    }
+
+    public String getRankupdatedate() {
+        return rankupdatedate;
+    }
+
+    public void setRankupdatedate(String rankupdatedate) {
+        this.rankupdatedate = rankupdatedate;
+    }
+
+    public String getInputid() {
+        return inputid;
+    }
+
+    public void setInputid(String inputid) {
+        this.inputid = inputid;
+    }
+
+    public String getMktcode01() {
+        return mktcode01;
+    }
+
+    public void setMktcode01(String mktcode01) {
+        this.mktcode01 = mktcode01;
+    }
+
+    public String getWatchrate() {
+        return watchrate;
+    }
+
+    public void setWatchrate(String watchrate) {
+        this.watchrate = watchrate;
+    }
+
+    public String getExpectrate() {
+        return expectrate;
+    }
+
+    public void setExpectrate(String expectrate) {
+        this.expectrate = expectrate;
+    }
+
+    public String getIsindraftdown() {
+        return isindraftdown;
+    }
+
+    public void setIsindraftdown(String isindraftdown) {
+        this.isindraftdown = isindraftdown;
+    }
+
+    public String getAfterdownrating() {
+        return afterdownrating;
+    }
+
+    public void setAfterdownrating(String afterdownrating) {
+        this.afterdownrating = afterdownrating;
+    }
+
+    public String getUpdatetimestamp() {
+        return updatetimestamp;
+    }
+
+    public void setUpdatetimestamp(String updatetimestamp) {
+        this.updatetimestamp = updatetimestamp;
+    }
+
+    public String getTim() {
+        return tim;
+    }
+
+    public void setTim(String tim) {
+        this.tim = tim;
+    }
+}
